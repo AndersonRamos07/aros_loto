e0d9150f64d6117461827776b598a4cc383df911
AndersonRamos07 <andersonramos07@live.com>
2022-01-22 01:47:38 -0300

    finalizando a correção de 21JAN2022

--- ps/script.js
+++ ps/script.js
@@ -15,6 +15,28 @@ const criaInput = (nome, etiqueta) =>{
     return div;
 }
 
+const criarForm = (parentesco) => {
+    var campo = document.createElement('fieldset');
+    if(parentesco[0] == 'F'){ campo.className = 'Filho'; }
+    else{ campo.className = parentesco; }
+    campo.setAttribute('id', parentesco)
+    var legenda = document.createElement('legend');
+        legenda.innerText = `INFORMAÇÕES DE CONTATO - ${parentesco}`;
+    var sobrenome = criaInput('lastName', 'Sobrenome');
+    var nome = criaInput('firstName', 'Nome');
+    var email = criaInput('email', 'Email');
+    var telefone = criaInput('phone', 'Telefone');
+        campo.appendChild(legenda);
+        campo.appendChild(sobrenome);
+        campo.appendChild(nome);
+        campo.appendChild(email);
+        campo.appendChild(telefone);
+    var form = document.querySelector('form');
+    var botaoDeEnviar = document.querySelector('#botao');
+    form.insertBefore(campo, botaoDeEnviar);
+    return true
+ }
+
 const conferirEscolhaConjuge = () => {
     var escolha = document.querySelector('input[type=radio]:checked').value;
     var divConjuge = document.querySelector('.Conjuge');
@@ -25,16 +47,16 @@ const conferirEscolhaConjuge = () => {
     return true;
 }
 
-const conferirEscolhaFilho = async () =>{
+const conferirEscolhaFilho = () =>{
     var quantos = document.querySelector('#qtdFilhos');
-    document.querySelector('#qtdFilhos').value = '';
     var escolha = document.querySelector('input[name=filho]:checked').value;
 
     if(escolha != '9'){
+        document.querySelector('#qtdFilhos').value = '';
         quantos.setAttribute('disabled', 'true');
         criarFieldSets(escolha);
     }
-    if (escolha == '9'){
+    if(escolha == '9'){
         console.log('aqui')
         quantos.removeAttribute('disabled');
         quantos.focus();
@@ -42,9 +64,12 @@ const conferirEscolhaFilho = async () =>{
 }
 
 const escolhaMaior3 = () =>{
-    var maisDe3 = document.querySelector('#qtdFilhos').value;
+    var campo = document.querySelector('#qtdFilhos');
+    var maisDe3 = campo.value;
     criarFieldSets(maisDe3);
+    campo.setAttribute('disabled', 'true');
 }
+
 
 const criarFieldSets = (pEscolha) => {
     var maisDe3 = document.querySelector('#qtdFilhos').value;
@@ -96,28 +121,6 @@ const criarFieldSets = (pEscolha) => {
     return true;
 }
 
-const criarForm = (parentesco) => {
-    var campo = document.createElement('fieldset');
-    if(parentesco[0] == 'F'){ campo.className = 'Filho'; }
-    else{ campo.className = parentesco; }
-    campo.setAttribute('id', parentesco)
-    var legenda = document.createElement('legend');
-        legenda.innerText = `INFORMAÇÕES DE CONTATO - ${parentesco}`;
-    var sobrenome = criaInput('lastName', 'Sobrenome');
-    var nome = criaInput('firstName', 'Nome');
-    var email = criaInput('email', 'Email');
-    var telefone = criaInput('phone', 'Telefone');
-        campo.appendChild(legenda);
-        campo.appendChild(sobrenome);
-        campo.appendChild(nome);
-        campo.appendChild(email);
-        campo.appendChild(telefone);
-    var form = document.querySelector('form');
-    var botaoDeEnviar = document.querySelector('#botao');
-    form.insertBefore(campo, botaoDeEnviar);
-    return true
- }
-
  const deveEscolher = async () =>{
      var campoDeEscolha = document.querySelector('#qtdFilhos');
      var valorDeEscolha = campoDeEscolha.value;
@@ -131,6 +134,7 @@ const criarForm = (parentesco) => {
           })
           if(qtdFilhos != 0){
               campoDeEscolha.value = qtdFilhos;
+              campoDeEscolha.setAttribute('disabled', 'true');
           }
           criarFieldSets(qtdFilhos);
      }
