4d3b99fd1cdc82b98b2c3394ad579a991dc52cc5
AndersonRamos07 <andersonramos07@live.com>
2022-01-23 02:57:38 -0300

    teste de bibliotecas de PDF

--- forms/addForm.js
+++ forms/addForm.js
@@ -1,12 +1,33 @@
 const puppeteer = require('puppeteer');
 
+const fs = require('fs');
+const pdfDoc = require('pdfkit');
+const blobStream = require('blob-stream');
+
+const { DownloaderHelper } = require('node-downloader-helper');
+
+var pdfFillForm = require('pdf-fill-form');
+
+var pdfFields = pdfFillForm.readSync('test.pdf');
+console.log(pdfFields);
+
+/*
+const doc = new pdfDoc;
+doc.pipe(fs.createWriteStream(__dirname + '/path/file.pdf')); 
+doc.addContent('page');
+doc.end();
+*/
+
+const urlPage = `https://www.uscis.gov/sites/default/files/document/forms/g-1145.pdf`;
+
 //#region NOVA_PAGINA
 const newPage = async (form) =>{
+
   const browser = await puppeteer.launch({ headless: false, slowMo: 150, args:[
     '--start-maximized' // you can also use '--start-fullscreen'
  ]});
   const page = await browser.newPage();
-  await page.goto(`https://www.uscis.gov/sites/default/files/document/forms/${form}.pdf`);
+  await page.goto(urlPage);
   page.waitForNavigation();
   return page;
 }
@@ -37,7 +58,11 @@ const add = async (form, dados) => {
   await page.keyboard.type(dados.email);
   await tab(1)
   await page.keyboard.type(dados.phone);
-  await tab(16);
+  //await tab(16);
+
+  const dl = new DownloaderHelper( urlPage , __dirname);
+    dl.on('end', () => console.log('Download Completed'))
+    dl.start();
 };
 
 const confirm = async (dados) =>{
