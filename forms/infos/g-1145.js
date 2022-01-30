27f5f20ce278f13b82116cbe7d800cc0f0da349f
Anderson Ramos <andersonramos07@live.com>
2022-01-29 22:22:08 -0300

    Atualizando o projeto

--- forms/infos/g-1145.js
+++
@@ -1,9 +0,0 @@
-exports.G1145_CAMPOS = {
-    'lastName':'form1[0].#subform[0].LastName[0]',
-    'firstName':'form1[0].#subform[0].FirstName[0]',
-    'middleName':'form1[0].#subform[0].MiddleName[0]',
-    'email':'form1[0].#subform[0].Email[0]',
-    'mobilePhone':'form1[0].#subform[0].MobilePhoneNumber[0]'
-}
-
-//module.exports = { G1145_CAMPOS }
--- notes.js
+++
@@ -1,7 +0,0 @@
-try {
-  await page.waitForSelector('.foo');
-} catch (e) {
-  if (e instanceof puppeteer.errors.TimeoutError) {
-    // Do something if this is a timeout.
-  }
-}
