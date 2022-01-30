835b931ba45b067686138b73de7e093e37c71365
Anderson Ramos <andersonramos07@live.com>
2022-01-29 21:35:37 -0300

    FINALMENTE! CONSEGUI! Agora é fazer com os outros formularios

--- forms/preencheForm.js
+++ forms/preencheForm.js
@@ -1,22 +1,39 @@
 const { PDFDocument } = require('pdf-lib');
 const { readFile, writeFile } = require('fs').promises;
+const path = require('path');
+
+const g1145 = require('./infos/g-1145')
+const i131 = require('./infos/i-131')
+const i140 = require('./infos/i-140')
+const i485 = require('./infos/i-485')
+const i765 = require('./infos/i-765')
+const eta750b = require('./infos/eta-750b')
 
 const conferirDados = (pDados)=>
 {
-  var novoDado = Object.entries(pDados)
-    console.log('<pDados>')
-    console.log(novoDado);
-    console.log('<pDados>')
+  var novoDado = Object.entries(pDados);
+  var novaChave = Object.keys(pDados);
+  var novoValor = Object.values(pDados)
 
-    const baseURL = 'https://www.uscis.gov/sites/default/files/document/forms/';
+  console.log('<pDados>')
+  console.log(novoDado);
+  console.log('<pDados>')
+  console.log('<novaChave>')
+  console.log(novaChave);
+  console.log('<novaChave>')
+  console.log('<novoValor>')
+  console.log(novoValor);
+  console.log('<novoValor>')
 
-    const paraCada = (pDados)=> 
-    {
-        var result = [];
-        for (var i=0; i < pDados.length; i++){
-            result.push(pDados.value);
-      }
-    }
+  const gForm = pDados.nForm[0];
+  const gUsuario = pDados.firstName;
+  const gOutput = path.join(__dirname +`/saida/${gForm}-${gUsuario}.pdf`);
+  const fileRoot = 'G-1145.pdf';
+
+  console.log(gOutput);
+  console.log('---------------------------------')
+
+  preenchePDF(fileRoot, pDados, gOutput);
 }
 
 async function preenchePDF(formArquivo, dados, formSaida) {
@@ -25,22 +42,53 @@ async function preenchePDF(formArquivo, dados, formSaida) {
 //  dados = informações dados pelo usuário,
 //  formSaida = formulário preenchido
 //#endregion
-    try {
-        const pdfDoc = await PDFDocument.load(await readFile(formArquivo));
+  try
+  {
+    const pdfDoc = await PDFDocument.load(await readFile(formArquivo));
 
-        const form = pdfDoc.getForm();
+    const nomesDosCampos = pdfDoc
+      .getForm()
+      .getFields()
+      .map((nomes) => nomes.getName());
 
-        form.getTextField('Text2').setText('John Smith');
+    const formulario = pdfDoc.getForm();
 
-        form.getCheckBox('Check Box7').check();
+    console.log('<<<g1145.lastName>>>')
+    console.log(g1145.lastName)
+    console.log('<<<g1145.lastName>>>')
+    console.log('<<<dados.lastName>>>')
+    console.log(dados.lastName)
+    console.log('<<<dados.lastName>>>')
 
-        const pdfBytes = await pdfDoc.save();
+    formulario.getTextField(g1145.G1145_CAMPOS.lastName).setText(dados.lastName);
+    formulario.getTextField(g1145.G1145_CAMPOS.firstName).setText(dados.lastName);
+    formulario.getTextField(g1145.G1145_CAMPOS.email).setText(dados.email);
+    formulario.getTextField(g1145.G1145_CAMPOS.mobilePhone).setText(dados.phone);
 
-        await writeFile(output, pdfBytes);
-        console.log('PDF created!');
-    } catch (err) {
-    console.log(err);
-    }
+
+    console.log('<<nomesDosCampos>>');
+    console.log(nomesDosCampos);
+    console.table(nomesDosCampos);
+    console.log('<<{nomesDosCampos}>>');
+    console.log({nomesDosCampos});
+
+    //form.getTextField();
+    
+    const pdfBytes = await pdfDoc.save();
+    
+    await writeFile(formSaida, pdfBytes);
+    
+    console.log('PDF created!');
+/*
+    form.getTextField('Text2').setText('John Smith');
+
+    form.getCheckBox('Check Box7').check();
+
+
+    */
+  } catch (err) {
+  console.log(err);
+  }
 }
 
 module.exports = { preenchePDF , conferirDados }
